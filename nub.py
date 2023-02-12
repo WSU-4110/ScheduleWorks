e47880bf2dcae604cea874c512944ddebfdaab86
MMazenA <Jazenjirza@gmail.com>
2023-02-12 18:36:49 -0500

    finished prereq list needs doc

--- nub.py
+++ nub.py
@@ -1,5 +1,7 @@
 """Wrapper class to maintain cookies from NU Banner API"""
 import requests
+from bs4 import BeautifulSoup
+import re
 
 
 class Nub:
@@ -43,7 +45,7 @@ class Nub:
         self.term = term
         return True
 
-    def get_subject_code(self, maximum=10, offset=1, search_term=""):
+    def get_subject_code(self, search_term, maximum=10, offset=1):
         """
         Generate a list of subjects and their codes using a filter.
 
@@ -90,32 +92,100 @@ class Nub:
 
         return response.json()
 
+    def reset_search(self):
+        """Resetting form."""
+        url = self.base_url + "/classSearch/resetDataForm"
+        self.session.post(url)
+
     def look_up(self, subject_code):
         """Search for a course given its subject code."""
 
+        self.reset_search()
         url = (
             self.base_url
             + f"/searchResults/searchResults?txt_subject={subject_code}&txt_term={self.term}&startDatepicker=&endDatepicker=&pageOffset=0&pageMaxSize=5000&sortColumn=subjectDescription&sortDirection=asc"
         )
-        print(url)
-        response = self.session.get(
-            url,
-        )
+        response = self.session.get(url)
 
         if response.status_code != 200:
             return [{"Error": response.status_code}]
 
         return response.json()
 
+    def get_course_reference(self, subject_code, course_code):
+        """Return class code for accessing its details."""
+        self.reset_search()
+        url = (
+            self.base_url
+            + f"/searchResults/searchResults?txt_subject={subject_code}&txt_term={self.term}&txt_courseNumber={course_code}&startDatepicker=&endDatepicker=&pageOffset=0&pageMaxSize=5000&sortColumn=subjectDescription&sortDirection=asc"
+        )
+        response = self.session.get(url, timeout=10)
+        if response.status_code != 200:
+            return [{"Error": response.status_code}]
+        elif response.json()["totalCount"] <= 0:
+            return [{"Error": "Cannot find class"}]
+
+        course_reference_number = response.json()["data"][0]["courseReferenceNumber"]
+        return course_reference_number
+
+    def get_prerequistes(self, subject_code, course_code):
+        """Obtain an array with course prequeistes given a course code and id."""
+        course_reference_number = self.get_course_reference(subject_code, course_code)
+        url = self.base_url + "/searchResults/getSectionPrerequisites"
+        body = {"term": self.term, "courseReferenceNumber": course_reference_number}
+        html_response = requests.post(url, data=body)
+        if html_response.status_code != 200:
+            return ["Error"]
+        parsed_html = BeautifulSoup(html_response.content, features="lxml")
+        prereq_table = parsed_html.body.find("table")
+
+        track = False
+        course_prereq_list = []
+        word = ""
+        # print(str(prereq_table))
+        for letter in str(prereq_table):
+            if letter == "\n" and word != "":
+                track = False
+                course_prereq_list.append(word)
+                word = ""
+            if track:
+                word += letter
+            if letter == ":":
+                track = True
+
+        print(course_prereq_list)
+
+        for i, course in enumerate(course_prereq_list[1::]):
+            course_prereq_list[i + 1] = {
+                "course": re.sub(r"[^a-zA-Z ]+", "", course).strip(),
+                "code": re.findall("\d+", course)[0],
+            }
+        course_prereq_list.pop(0)
+        print(course_prereq_list)
+
+        known_conversion = {}
+        for course_data in course_prereq_list:
+            course_name = course_data["course"]
+            if course_name in known_conversion.keys():
+                course_data["course"] = known_conversion[course_name]
+            else:
+                code = self.get_subject_code(course_name)[0]["code"]
+                known_conversion[course] = code
+                course_data["course"] = code
+
+        print(course_prereq_list)
+
 
 def main():
     """Example usage."""
-    pre_reqs = Nub("https://registration.wayne.edu/StudentRegistrationSsb/ssb")
-    print(pre_reqs.get_terms())
-    pre_reqs.set_term("202301")
-    pre_reqs.enable_search()
-    lookup = pre_reqs.look_up("CSC")
-    print(lookup)
+    nub = Nub("https://registration.wayne.edu/StudentRegistrationSsb/ssb")
+    print(nub.get_terms())
+    nub.set_term("202301")
+    nub.enable_search()
+    print(nub.get_subject_code("Basic Engineering")[0])
+    # lookup = pre_reqs.look_up("CSC")
+    # nub.pre_reqs("CSC", "2200")
+    nub.get_prerequistes("CSC", "2200")
 
 
 if __name__ == "__main__":
