f85a9db37f5747a8071fd13c5c4581299bfa1982
MizanulH489 <mizanulhaque489@gmail.com>
2023-02-16 00:37:05 -0500

    python file that parses info from JSON file for user's grad requirements, initial push

--- parseinfo.py
+++ parseinfo.py
@@ -2,3 +2,71 @@ import json
 
 #Open jsonfile
 open_f=open('data.json')
+data = json.load(open_f)
+
+
+#Hold courses in each category for grad requiremnt
+MATH_courses = []
+PHY_courses = []
+CS_courses = []
+GE_courses = []
+
+## Iterate over each course
+for course in data ['courses']:
+    # Extract course properties
+    name = course['name']
+    code = course['code']
+    discipline = code.split()[0]
+    number = code.split()[1]
+    category = course['category'] #split into category an credigt
+    credits = course['credits']
+
+    # Categorize course by discipline and number
+    if discipline == 'MATH':
+        MATH_courses.append({
+            'name': name,
+            'discipline': discipline,
+            'number': number,
+            'category': category,
+            'credits': credits
+        })
+    elif discipline == 'PHYS':
+        PHY_courses.append({
+            'name': name,
+            'discipline': discipline,
+            'number': number,
+            'category': category,
+            'credits': credits
+        })
+    elif discipline == 'CSCI':
+        CS_courses.append({
+            'name': name,
+            'discipline': discipline,
+            'number': number,
+            'category': category,
+            'credits': credits
+        })
+    else:
+        GE_courses.append({
+            'name': name,
+            'discipline': discipline,
+            'number': number,
+            'category': category,
+            'credits': credits
+        })
+
+# Calculate total number of credits 
+TOT_credits = sum([course['credits'] for course in CS_courses])
+
+# Create a dictionary with all categorized courses and total credits
+output_data = {
+    'Math courses': MATH_courses,
+    'Physics courses': PHY_courses,
+    'Computer Science courses': CS_courses,
+    'General Education courses': GE_courses,
+    'Total credits needed for a degree': TOT_credits
+}
+
+# Write the output to a JSON 
+with open('output.json', 'w') as f:
+    json.dump(output_data, f, indent=4)
