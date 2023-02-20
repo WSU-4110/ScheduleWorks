0490f19b0e8dab67c938b98214ae0351ae585a50
MizanulH489 <mizanulhaque489@gmail.com>
2023-02-19 21:01:37 -0500

    updated parse to get course req

--- parseinfo.py
+++ parseinfo.py
@@ -6,8 +6,12 @@ data = json.load(open_f)
 
 
 
-#Grab discipline from key course
+#Grab discipline from course and course requirements
 courses = {}
+#store list of course req
+course_req = {}
+
+
 
 #Show all keys
 print(data.keys())
@@ -25,12 +29,29 @@ for course in data["classInformation"]["classArray"]: #extract
                      courses[discipline][number] = credits
 
 
-#Key of blockarray that contains classesAppliedtoRule and class arrar
+#Key of blockarray that contains classesAppliedtoRule and class array
 for course in data["blockArray"]:
     else_part = course.get("ifPart", {}).get("elsePart", {})
     rule_array = else_part.get("ruleArray", [])
     for rule in rule_array:
         for class_info in rule.get("classesAppliedToRule", {}).get("classArray", []):
+            discipline = class_info["discipline"]
+            number = class_info["number"]
+            credits = int(class_info["credits"])
+            if discipline not in courses:
+                courses[discipline] = {}
+            if number not in courses[discipline]:
+                courses[discipline][number] = credits
+
+#get course req
+# Iterate through blockArray
+for course in data["blockArray"]:
+    else_part = course.get("ifPart", {}).get("elsePart", {})
+    rule_array = else_part.get("ruleArray", [])
+    for rule in rule_array:
+        for class_info in rule.get("classesAppliedToRule", {}).get("classArray", []):
+            if class_info["letterGrade"] == "INPR" or class_info["percentComplete"] != "100" or class_info["percentComplete"] == "0":
+                course_req.append(class_info)
             discipline = class_info["discipline"]
             number = class_info["number"]
             credits = int(class_info["credits"])
@@ -45,5 +66,4 @@ Tot_credits = sum(sum(courses[discipline].values()) for discipline in courses)
 
 # Write the output to a JSON file(output)
 with open("output.json", "w") as f:
-    json.dump({"courses": courses, "Tot_credits": Tot_credits}, f, indent=4) 
-
+    json.dump({"Course History": courses, "Tot_credits": Tot_credits,"Course Requirements": course_req}, f, indent=4) 
