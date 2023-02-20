ba3a7b8677e481c96adf9e05f59c4a71b296a46d
MMazenA <Jazenjirza@gmail.com>
2023-02-20 03:00:17 -0500

    removing privateInfo

--- schedule-works-be/manual_get_data.py
+++ schedule-works-be/manual_get_data.py
@@ -9,7 +9,7 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 from bs4 import BeautifulSoup
-import privateInfo
+# import privateInfo
 import devolpment_shortcut
 
 
