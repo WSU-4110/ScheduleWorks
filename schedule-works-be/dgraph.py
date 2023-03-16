fc6bcf7fad4e9d7afa34c670934b889b758bdda4
MizanulH489 <mizanulhaque489@gmail.com>
2023-03-16 13:43:47 -0400

    added topograph sort for dgraph so it shows order

--- schedule-works-be/dgraph.py
+++ schedule-works-be/dgraph.py
@@ -1,67 +1,32 @@
-"""Functions for interacting with directed graph structure."""
-
 import networkx as nx
 import matplotlib.pyplot as plt
 
-
-def course_sum(graph: nx.DiGraph, course: str, memo: dict) -> set:
+def topological_sort(graph: nx.DiGraph) -> list:
     """
-    Determine a given course's dependency list. See make_priority_queue() to use properly.
-
-    Wrapper function to clarify args used during recursion.
-    Functions best when courses are fed in order from least expected dependency to most.
-
+    Return a list of nodes in topological order.
 
     Args:
-    G: A directed graph containing courses and edges that point towards a courses prequisite.
-    course: The course that is being analysed for priority count.
-    memo: Memoization dict to speed up complex prerequisties
+        graph: A directed graph.
+
+    Returns:
+        A list of nodes in topological order.
     """
-    included_courses = set()
+    topo_order = []
+    zero_in_degree_nodes = [n for n, d in graph.in_degree() if d == 0]
 
-    def course_summate(course_id: str) -> None:
-        """Recursive function to return a list of prereq courses given a course."""
+    while zero_in_degree_nodes:
+        node = zero_in_degree_nodes.pop(0)
+        topo_order.append(node)
 
-        if course_id not in included_courses:
-            included_courses.update([course_id])
-        if course_id in memo.keys():
-            included_courses.update(memo[course_id])
-            return
-        if len(graph.in_edges(course_id)) == 0:
-            included_courses.update([course_id])
-            return
-        for dependent_course in graph.in_edges(course_id):
-            dependent_course = dependent_course[0]
-            if dependent_course not in included_courses:
-                included_courses.update([dependent_course])
-                course_summate(dependent_course)
-        return
+        for successor in graph.successors(node):
+            graph.remove_edge(node, successor)
+            if graph.in_degree(successor) == 0:
+                zero_in_degree_nodes.append(successor)
 
-    course_summate(course)
-    return included_courses
-
-
-def make_priority_queue(graph: nx.DiGraph) -> dict:
-    """
-    Produce a priority queue of courses from a directed graph.
-
-    TODO:
-    return numeric list.
-    ensure priority when 2 classes are equal length.
-    """
-    sorted_by_level = sorted(
-        list(graph.nodes()), key=lambda x: int(x[3:]), reverse=True
-    )
-
-    memo = {}
-    for node in sorted_by_level:
-        dependent_courses = course_sum(graph, node, memo)
-
-        memo[node] = dependent_courses
-        print(node, ": ", len(dependent_courses))
-
-    return memo
-
+    if graph.edges():
+        raise ValueError("Graph is not acyclic")
+    else:
+        return topo_order
 
 def show_graph(graph):
     """Print a graph out to show node connections."""
@@ -98,8 +63,9 @@ def main():
         ]
     )
     show_graph(graph)
-    make_priority_queue(graph)
 
+    topo_order = topological_sort(graph)
+    print("Topological order:", topo_order)
 
 if __name__ == "__main__":
     main()
