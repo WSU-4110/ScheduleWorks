a358ed60d298f0c189eece3ad78bb0b7585d92b2
ChristopherForkin36 <123266468+ChristopherForkin36@users.noreply.github.com>
2023-04-15 13:59:44 -0400

    Testing files

---
+++ devolpment_shortcut_testing.py
@@ -0,0 +1,23 @@
+import unittest
+import devolpment_shortcut as retrive_data
+
+class test_devolpment_shortcut(unittest.TestCase):
+
+    def set_devolpment_shortcut():
+        refresh_token = 'testrefresh'
+        token = 'testtoken'
+        name = 'testname'
+
+    def test_retrive_data(refresh_token: str, token: str, name: str) -> None:
+        actualrefresh = refresh_token
+        actualtoken = token
+        actualname = name
+        expectedrefresh = 'testrefresh'
+        expectedtoken = 'testtoken'
+        expectedname = 'testname'
+        refresh_token.assertEqual(expectedrefresh, actualrefresh)
+        token.assertEqual(expectedtoken, actualtoken)
+        name.assertEqual(expectedname, actualname)
+
+if __name__ == '__main__':
+    unittest.main()
