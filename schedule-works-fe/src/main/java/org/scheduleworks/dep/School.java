bc17932cdec2fba579dd0ea2c519d94c07335fcf
Faizan Bhatti <FaizanaliB2@gmail.com>
2023-04-16 14:36:25 -0400

    setters and getters

---
+++ schedule-works-fe/src/main/java/org/scheduleworks/dep/School.java
@@ -0,0 +1,112 @@
+package org.scheduleworks.dep;
+
+public class School {
+    private String courseName;
+    private String courseCode;
+    private String courseSubject;
+    private String campus;
+    private String startTime;
+    private String endTime;
+    private boolean monday;
+    private boolean tuesday;
+    private boolean wednesday;
+    private boolean thursday;
+    private boolean friday;
+    private int openSeats;
+
+    public String getCourseName() {
+        return courseName;
+    }
+
+    public void setCourseName(String courseName) {
+        this.courseName = courseName;
+    }
+
+    public String getCourseCode() {
+        return courseCode;
+    }
+
+    public void setCourseCode(String courseCode) {
+        this.courseCode = courseCode;
+    }
+
+    public String getCourseSubject() {
+        return courseSubject;
+    }
+
+    public void setCourseSubject(String courseSubject) {
+        this.courseSubject = courseSubject;
+    }
+
+    public String getCampus() {
+        return campus;
+    }
+
+    public void setCampus(String campus) {
+        this.campus = campus;
+    }
+
+    public String getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(String startTime) {
+        this.startTime = startTime;
+    }
+
+    public String getEndTime() {
+        return endTime;
+    }
+
+    public void setEndTime(String endTime) {
+        this.endTime = endTime;
+    }
+
+    public boolean isMonday() {
+        return monday;
+    }
+
+    public void setMonday(boolean monday) {
+        this.monday = monday;
+    }
+
+    public boolean isTuesday() {
+        return tuesday;
+    }
+
+    public void setTuesday(boolean tuesday) {
+        this.tuesday = tuesday;
+    }
+
+    public boolean isWednesday() {
+        return wednesday;
+    }
+
+    public void setWednesday(boolean wednesday) {
+        this.wednesday = wednesday;
+    }
+
+    public boolean isThursday() {
+        return thursday;
+    }
+
+    public void setThursday(boolean thursday) {
+        this.thursday = thursday;
+    }
+
+    public boolean isFriday() {
+        return friday;
+    }
+
+    public void setFriday(boolean friday) {
+        this.friday = friday;
+    }
+
+    public int getOpenSeats() {
+        return openSeats;
+    }
+
+    public void setOpenSeats(int openSeats) {
+        this.openSeats = openSeats;
+    }
+}
