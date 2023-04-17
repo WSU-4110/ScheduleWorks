45f12c6d0c2f07f2b4629fb75e6d68896f9e92e1
Faizan Bhatti <FaizanaliB2@gmail.com>
2023-04-17 03:36:17 -0400

    The FAQ Page and reaching at discord

--- schedule-works-fe/src/main/java/org/scheduleworks/dep/InterfaceController.java
+++ schedule-works-fe/src/main/java/org/scheduleworks/dep/InterfaceController.java
@@ -17,7 +17,7 @@ import java.util.Scanner;
 import java.io.BufferedWriter;
 import java.io.FileWriter;
 import javafx.scene.text.Text;
-import java.io.IOException; 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.io.BufferedReader;
 import java.io.FileReader;
@@ -29,7 +29,7 @@ import java.net.URI;
 import java.awt.Desktop;
 
 public class InterfaceController extends InterfaceMain {
-    //  BUTTON LIST
+    // BUTTON LIST
     @FXML
     private Button btn_login_tab;
     @FXML
@@ -44,7 +44,7 @@ public class InterfaceController extends InterfaceMain {
     private ToggleButton toggle_selenium_bg;
     @FXML
     private ToggleButton toggle_save_cookies;
-   
+
     @FXML
     private TextField code;
     @FXML
@@ -57,34 +57,33 @@ public class InterfaceController extends InterfaceMain {
     private Button btn_show_graph;
     @FXML
     private Button btn_show_graph_full;
-   
+
     @FXML
     private TableView<Course> tableCourses;
- 
-    //  IMAGE LIST
+
+    // IMAGE LIST
     @FXML
     private ImageView graphView;
-    
-    //  GLOBAL VARIABLES
+
+    // GLOBAL VARIABLES
     private static Stage stg;
     private double x, y = 0;
     static privateInfo userInfo = new privateInfo();
 
-    //  LOGIN TAB
+    // LOGIN TAB
     public void pressLoginTab() throws Exception {
         changeScene("InterfaceLogin.fxml");
 
-        //changeUserText();         //  FIX THIS if possible, remove/comment out otherwise
+        // changeUserText(); // FIX THIS if possible, remove/comment out otherwise
     }
 
     public void changeUserText() throws Exception {
         if (userInfo.getUsername() == "") {
             userText.setText("Not currently logged in ⚠");
-            //btn_login_tab.setText("Login");
-        }
-        else {
+            // btn_login_tab.setText("Login");
+        } else {
             userText.setText("Welcome " + userInfo.getUsername());
-            //btn_login_tab.setText("Welcome " + userInfo.getUsername());
+            // btn_login_tab.setText("Welcome " + userInfo.getUsername());
         }
     }
 
@@ -93,9 +92,9 @@ public class InterfaceController extends InterfaceMain {
         userInfo.setPassword(password.getText().toString());
         changeUserText();
 
-        System.out.println(userInfo.getUsername());     //  Test print, can be removed later
-        
-        Thread t = new Thread(new pythonExecute(userInfo.getUsername(),userInfo.getPassword()));
+        System.out.println(userInfo.getUsername()); // Test print, can be removed later
+
+        Thread t = new Thread(new pythonExecute(userInfo.getUsername(), userInfo.getPassword()));
         t.start();
 
         username.clear();
@@ -107,8 +106,7 @@ public class InterfaceController extends InterfaceMain {
         if (isSelected) {
             toggle_selenium_bg.setText("On");
             userInfo.setSeleniumToggle(true);
-        }
-        else {
+        } else {
             toggle_selenium_bg.setText("Off");
             userInfo.setSeleniumToggle(false);
         }
@@ -119,138 +117,134 @@ public class InterfaceController extends InterfaceMain {
         if (isSelected) {
             toggle_save_cookies.setText("On");
             userInfo.setSaveCookiesToggle(true);
-        }
-        else {
+        } else {
             toggle_save_cookies.setText("Off");
             userInfo.setSaveCookiesToggle(false);
         }
     }
 
-    //  RETRIEVE COURSES TAB
+    // RETRIEVE COURSES TAB
     public void retrieveCoursesTab() throws Exception {
         changeScene("InterfaceRetrieve.fxml");
 
-
-
-
-        /*   OLD CODE, DELETE LATER
-        taCoursesTaken.clear();
-        File file = new File("C:\\Program Files\\ScheduleWorks\\data\\courseHistory.txt");
-        try {
-            if (file.createNewFile()) {
-                System.out.println("File created: " + file.getName());
-            } else {
-                System.out.println("File already exists.");
-            }
-        } catch (IOException e) {
-            System.out.println("An error occurred.");
-            e.printStackTrace();
-          }
-
-       try{
-        Scanner scan = new Scanner(file);
-        if (file.length()==0){
-            taCoursesTaken.clear();
-            taCoursesTaken.setPromptText("Please login and submit the 2FA Code");
-        }
-        while(scan.hasNextLine()){
-            taCoursesTaken.appendText(scan.nextLine() + "\n");
-            }
-        } 
-        catch (FileNotFoundException e){
-        e.printStackTrace();
-       }
-       */
+        /*
+         * OLD CODE, DELETE LATER
+         * taCoursesTaken.clear();
+         * File file = new
+         * File("C:\\Program Files\\ScheduleWorks\\data\\courseHistory.txt");
+         * try {
+         * if (file.createNewFile()) {
+         * System.out.println("File created: " + file.getName());
+         * } else {
+         * System.out.println("File already exists.");
+         * }
+         * } catch (IOException e) {
+         * System.out.println("An error occurred.");
+         * e.printStackTrace();
+         * }
+         * 
+         * try{
+         * Scanner scan = new Scanner(file);
+         * if (file.length()==0){
+         * taCoursesTaken.clear();
+         * taCoursesTaken.setPromptText("Please login and submit the 2FA Code");
+         * }
+         * while(scan.hasNextLine()){
+         * taCoursesTaken.appendText(scan.nextLine() + "\n");
+         * }
+         * }
+         * catch (FileNotFoundException e){
+         * e.printStackTrace();
+         * }
+         */
     }
-    public void setUpTable(){
-        
-        TableColumn<Course, String>attributeColumn = new TableColumn<>("Discipline");
+
+    public void setUpTable() {
+
+        TableColumn<Course, String> attributeColumn = new TableColumn<>("Discipline");
         attributeColumn.setMinWidth(50);
         attributeColumn.setCellValueFactory(new PropertyValueFactory<>("discipline"));
-        
 
-        TableColumn<Course, String>passColumn = new TableColumn<>("Passed");
+        TableColumn<Course, String> passColumn = new TableColumn<>("Passed");
         passColumn.setMinWidth(60);
         passColumn.setCellValueFactory(new PropertyValueFactory<>("pass"));
-        
 
-        TableColumn<Course, String>nameColumn = new TableColumn<>("Name");
+        TableColumn<Course, String> nameColumn = new TableColumn<>("Name");
         nameColumn.setMinWidth(200);
         nameColumn.setCellValueFactory(new PropertyValueFactory<>("name"));
-        
 
-        TableColumn<Course, String>creditColumn = new TableColumn<>("Credit");
+        TableColumn<Course, String> creditColumn = new TableColumn<>("Credit");
         creditColumn.setMinWidth(40);
         creditColumn.setCellValueFactory(new PropertyValueFactory<>("credit"));
-        
 
-        TableColumn<Course, String>numberColumn = new TableColumn<>("Number");
+        TableColumn<Course, String> numberColumn = new TableColumn<>("Number");
         numberColumn.setMinWidth(50);
         numberColumn.setCellValueFactory(new PropertyValueFactory<>("number"));
-    
-        
+
         tableCourses.setItems(getCoursedList());
-        tableCourses.getColumns().addAll(attributeColumn,numberColumn,creditColumn,nameColumn,passColumn);
+        tableCourses.getColumns().addAll(attributeColumn, numberColumn, creditColumn, nameColumn, passColumn);
     }
 
     public void pressRetrieveStartButton() throws Exception {
         System.out.println("RETRIEVE START");
         // taCoursesTaken.clear();
         setUpTable();
-    //     //  No login info present
-    //     if (userInfo.getUsername() == "" || userInfo.getPassword() == "") {
-    //         taCoursesTaken.appendText("Username and/or password not entered\nEnter login info in the \"Login\" tab");
-    //         return;
-    //     }
+        // // No login info present
+        // if (userInfo.getUsername() == "" || userInfo.getPassword() == "") {
+        // taCoursesTaken.appendText("Username and/or password not entered\nEnter login
+        // info in the \"Login\" tab");
+        // return;
+        // }
 
-    //     File file = new File("C:\\Program Files\\ScheduleWorks\\data\\courseHistory.txt");
-    //     try {
-    //         if (file.createNewFile()) {
-    //             System.out.println("File created: " + file.getName());
-    //         } else {
-    //             System.out.println("File already exists.");
-    //         }
-    //     } catch (IOException e) {
-    //         System.out.println("An error occurred.");
-    //         e.printStackTrace();
-    //       }
+        // File file = new File("C:\\Program
+        // Files\\ScheduleWorks\\data\\courseHistory.txt");
+        // try {
+        // if (file.createNewFile()) {
+        // System.out.println("File created: " + file.getName());
+        // } else {
+        // System.out.println("File already exists.");
+        // }
+        // } catch (IOException e) {
+        // System.out.println("An error occurred.");
+        // e.printStackTrace();
+        // }
 
-    //    try{
-    //     Scanner scan = new Scanner(file);
-    //     if (file.length()==0){
-    //         taCoursesTaken.clear();
-    //         taCoursesTaken.setPromptText("Please login and submit the 2FA Code");
-    //     }
-    //     while(scan.hasNextLine()){
-    //         taCoursesTaken.appendText(scan.nextLine() + "\n");
-    //         }
-    //     } 
-    //     catch (FileNotFoundException e){
-    //     e.printStackTrace();
-    //    }
+        // try{
+        // Scanner scan = new Scanner(file);
+        // if (file.length()==0){
+        // taCoursesTaken.clear();
+        // taCoursesTaken.setPromptText("Please login and submit the 2FA Code");
+        // }
+        // while(scan.hasNextLine()){
+        // taCoursesTaken.appendText(scan.nextLine() + "\n");
+        // }
+        // }
+        // catch (FileNotFoundException e){
+        // e.printStackTrace();
+        // }
 
     }
 
     public void submitCode() {
         System.out.println("Code submitted");
-        try{
-            BufferedWriter br = new BufferedWriter(new FileWriter(new File("C:\\Program Files\\ScheduleWorks\\schedule-works-be\\2fa_code.txt")));
+        try {
+            BufferedWriter br = new BufferedWriter(
+                    new FileWriter(new File("C:\\Program Files\\ScheduleWorks\\schedule-works-be\\2fa_code.txt")));
             br.write(code.getText().toString());
             br.write("done");
             br.close();
-        }
-        catch (java.io.IOException e){
+        } catch (java.io.IOException e) {
             e.printStackTrace();
         }
         code.setPromptText(code.getText().toString());
         code.clear();
     }
 
-    public void viewCourseHistory(){
+    public void viewCourseHistory() {
         stg.close();
     }
 
-    //  RETRIEVE COURSES TAB
+    // RETRIEVE COURSES TAB
     public void createScheduleTab() throws Exception {
 
         changeScene("InterfaceCreateSchedule.fxml");
@@ -258,12 +252,11 @@ public class InterfaceController extends InterfaceMain {
 
     public void showGraphFull() throws Exception {
         System.out.println(getPane().widthProperty().getValue());
-        graphView.setFitWidth(getPane().widthProperty().getValue()/2); 
-        graphView.setLayoutX(getPane().widthProperty().getValue()/5);
-        try{
-        graphView.setImage(new Image(getClass().getClassLoader().getResourceAsStream("graph_full.png")));
-        }
-        catch(java.lang.RuntimeException e){
+        graphView.setFitWidth(getPane().widthProperty().getValue() / 2);
+        graphView.setLayoutX(getPane().widthProperty().getValue() / 5);
+        try {
+            graphView.setImage(new Image(getClass().getClassLoader().getResourceAsStream("graph_full.png")));
+        } catch (java.lang.RuntimeException e) {
             System.out.println("Couldn't find image");
             return;
         }
@@ -271,20 +264,20 @@ public class InterfaceController extends InterfaceMain {
         btn_show_graph_full.setVisible(false);
 
     }
+
     public void showGraph() throws Exception {
         System.out.println(getPane().widthProperty().getValue());
-        graphView.setFitWidth(getPane().widthProperty().getValue()/2); 
-        graphView.setLayoutX(getPane().widthProperty().getValue()/5);
-        try{
+        graphView.setFitWidth(getPane().widthProperty().getValue() / 2);
+        graphView.setLayoutX(getPane().widthProperty().getValue() / 5);
+        try {
             graphView.setImage(new Image(getClass().getClassLoader().getResourceAsStream("graph_small.png")));
-        }
-        catch(java.lang.RuntimeException e){
+        } catch (java.lang.RuntimeException e) {
             System.out.println("Couldn't find image");
             return;
         }
         btn_show_graph.setVisible(false);
         btn_show_graph_full.setVisible(false);
-        
+
     }
 
     public ArrayList<ArrayList<String>> getCourseData() {
@@ -292,52 +285,49 @@ public class InterfaceController extends InterfaceMain {
 
         File file = new File("C:\\Program Files\\ScheduleWorks\\data\\courseHistory.txt");
         BufferedReader reader;
-        int iter=0;
+        int iter = 0;
         ArrayList<String> tempList = new ArrayList<String>();
 
-		try {
-			reader = new BufferedReader(new FileReader(file));
-			String line = reader.readLine();
+        try {
+            reader = new BufferedReader(new FileReader(file));
+            String line = reader.readLine();
 
-			while (line != null) {
-				// System.out.println(line);
+            while (line != null) {
+                // System.out.println(line);
                 int col = iter % 5;
-                if(col%5==0 && tempList.size()>0){
-                    arrList.add((ArrayList<String>)tempList.clone());
+                if (col % 5 == 0 && tempList.size() > 0) {
+                    arrList.add((ArrayList<String>) tempList.clone());
                     tempList.clear();
                 }
                 tempList.add(line);
-				line = reader.readLine();
-                iter+=1;
-			}
+                line = reader.readLine();
+                iter += 1;
+            }
 
-			reader.close();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+            reader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         // System.out.println(Arrays.deepToString(arrList.toArray()));
 
         return arrList;
 
     }
 
-    public ObservableList<Course> getCoursedList(){
+    public ObservableList<Course> getCoursedList() {
         ObservableList<Course> data = FXCollections.observableArrayList();
 
-        for(ArrayList<String> tempList: getCourseData()){
+        for (ArrayList<String> tempList : getCourseData()) {
             // System.out.println(tempList.get(4)+tempList.get(3)+tempList.get(2)+tempList.get(0)+tempList.get(1));
-            data.add(new Course(tempList.get(4),tempList.get(3),tempList.get(2),tempList.get(0),tempList.get(1)));
+            data.add(new Course(tempList.get(4), tempList.get(3), tempList.get(2), tempList.get(0), tempList.get(1)));
         }
 
-    
         return data;
     }
 
-
-    
-
-    //  Close application
-    @FXML private javafx.scene.control.Button Close_button;
+    // Close application
+    @FXML
+    private javafx.scene.control.Button Close_button;
 
     public void closeApplication() throws Exception {
         Stage stage = (Stage) Close_button.getScene().getWindow();
@@ -347,14 +337,37 @@ public class InterfaceController extends InterfaceMain {
         System.exit(0);
     }
 
-
     public void pressHelp() throws Exception {
         changeScene("Help.fxml");
-        
-    }
-    public void EmailPage() throws Exception{
-        System.out.println("Link clicked");
-        Desktop.getDesktop().browse(new URI("https://mail.google.com/mail"));
-    }
+
     }
 
+    public void FaqPage() throws Exception {
+        changeScene("Faq.fxml");
+
+    }
+
+    public void firstMenu() throws Exception {
+        changeScene("first.fxml");
+
+    }
+
+    public void secondMenu() throws Exception {
+        changeScene("second.fxml");
+
+    }
+
+    public void thirdMenu() throws Exception {
+        changeScene("third.fxml");
+    }
+
+    public void fourthMenu() throws Exception {
+        changeScene("fourth.fxml");
+    }
+
+    public void EmailPage() throws Exception {
+        System.out.println("Link clicked");
+        Desktop.getDesktop().browse(new URI("https://github.com/WSU-4110/ScheduleWorks/discussions/59"));
+    }
+
+}
