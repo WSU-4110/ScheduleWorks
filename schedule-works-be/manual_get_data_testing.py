149b902deed36ae35a9c7ca4f7e3b7c1ea0aa9fd
MMazenA <jazenjirza@gmail.com>
2023-04-24 21:44:22 -0400

    clean up

---
+++ schedule-works-be/manual_get_data_testing.py
@@ -0,0 +1,20 @@
+# import unittest
+# import manual_get_data as get_json_data
+
+# class test_manual_get_data(unittest.TestCase):
+
+#     def set_manual_get_data():
+#         sid = 'gy3531'
+#         passwd = 'password123'
+
+#     def test_get_json_data(self, sid, passwd):
+#         actualsid = sid
+#         actualpasswd = passwd
+#         expectedsid = 'gy3531'
+#         expectedpasswd = 'password123'
+#         sid.assertEqual(expectedsid, actualsid)
+#         passwd.assertEqual(expectedpasswd, actualpasswd)
+
+
+# if __name__ == '__main__':
+#     unittest.main()
